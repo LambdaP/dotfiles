18dc20a82ca841ae9174d85e03827e7a3e6eb9bf
Patrick Lambein <patrick@lambein.name>
2016-01-06 12:33:46 +0100

    Lots of things.
    Refactor vimrc, add nvim.config, update install.sh to take those changes
    into account, minor changes to zsh.config

--- install.sh
+++ install.sh
@@ -12,9 +12,26 @@ fi
 
 ln -sf $CURR_DIR/vimrc ~/.vimrc
 
-echo "Install vim plugins."
-mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Neovim
+
+echo "Link nvim/init.vim."
+if [-e ~/.config/nvim/init.vim ]
+then
+        mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.pre_script
+fi
+
+mkdir -p ~/.config/nvim
+
+ln -sf $CURR_DIR/.config/init.vim ~/nvim.config
+
+echo "Install vim-plug for vim."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Install vim-plug for neovim."
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
 echo "Install oh-my-zsh and the Elisa theme."
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.config/oh-my-zsh
--- install_mac.sh
+++ install_mac.sh
@@ -5,3 +5,6 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 echo "Install brew-cask."
 brew install caskroom/cask/brew-cask
+
+echo "Now run visudo and add the line:"
+echo "Defaults tty_tickets # Bound sudo privilege to a TTY."
