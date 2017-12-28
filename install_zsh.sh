9fe03d8f7bc93b6324c68449dc84266478bf6c9c
Patrick Lambein <patrick@lambein.name>
2017-12-28 12:02:22 +0100

    Clean up zsh setup process (1/n)

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
+# FIXME As it is, this script does not set up zsh properly
+# #!/bin/sh
 
 CURR_DIR=$(pwd)
+ZSH_CONFIG_DIR=$HOME/.config/zsh
 
-mkdir -p ~/.config/zsh
+mkdir -p ZSH_CONFIG_DIR
 
-echo "Install oh-my-zsh and the Elisa theme."
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.config/oh-my-zsh
-ln -sf $CURR_DIR/zsh/elisa.zsh-theme ~/.config/oh-my-zsh/themes
-mkdir -p ~/bin
-ln -sf $CURR_DIR/zsh/batcharge.py ~/bin
+stow -v zsh --target=$HOME
+stow -v zsh --target=$HOME --dir=$CURR_DIR/local
 
 echo "Install zshrc."
 if [ -e ~/.zshrc ]
@@ -16,10 +15,14 @@ then
 	mv ~/.zshrc ~/.config/zshrc.pre_script
 fi
 
-echo "source $HOME/.config/zsh/zsh.config" > ~/.zshrc
+echo "source $ZSH_CONFIG_DIR/zsh.config" > ~/.zshrc
 
-ln -sf $CURR_DIR/zsh/zsh.config ~/.config/zsh/
-ln -sf $CURR_DIR/zsh/private/*  ~/.config/zsh/
+# TODO: this should be replaced with deployment of the theme as it is nowadays.
+# echo "Install oh-my-zsh and the Elisa theme."
+# git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.config/oh-my-zsh
+# ln -sf $CURR_DIR/zsh/elisa.zsh-theme ~/.config/oh-my-zsh/themes
+# mkdir -p ~/bin
+# ln -sf $CURR_DIR/zsh/batcharge.py ~/bin
 
 echo "Change default shell to zsh."
 chsh -s /bin/zsh
