ed2d73e900b62e766ca04d0d7b18f5e6ca69df4f
Patrick Lambein <patrick@lambein.name>
2020-11-04 10:56:55 +0100

    [zsh] comment out Zim

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,28 +1,24 @@
 # #!/bin/sh
 
-# NOTE: you should check whether /etc/zshenv is fine with you
-
-#if [ -z ${ZDOTDIR+x} ] then
-#  # on OSX
-#  SYSTEM_ZSH_ENV=/etc/zshenv
-#
-#  [ -f $SYSTEM_ZSH_ENV ] && sudo touch $SYSTEM_ZSH_ENV
-#  sudo echo "export ZDOTDIR=$HOME/.config/zsh" >> $SYSTEM_ZSH_ENV
-#fi
+[ -z ${XDG_CONFIG_HOME+x} ] && XDG_CONFIG_HOME=$HOME/.config
 
 ## Ensure ZSH config directory exists
+### NOTE: you should check whether /etc/zshenv is fine with you
 
-[ -z ${XDG_CONFIG_HOME+x} ] && XDG_CONFIG_HOME=$HOME/.config
-[ -z ${ZDOTDIR+x} ]         && ZDOTDIR=$XDG_CONFIG_HOME/zsh   && mkdir -p $ZDOTDIR
+if [ -z ${ZDOTDIR+x} ]; then
+  # on OSX
+  SYSTEM_ZSH_ENV=/etc/zshenv
 
-DOTFILES=$(pwd)
+  [ -f $SYSTEM_ZSH_ENV ] && sudo touch $SYSTEM_ZSH_ENV
+  sudo echo "export ZDOTDIR=$HOME/.config/zsh" >> $SYSTEM_ZSH_ENV
+
+  ZDOTDIR=$XDG_CONFIG_HOME/zsh
+  mkdir -p $ZDOTDIR
+fi
 
 # Symlink zsh config files
 
 stow -v --target=$ZDOTDIR --dotfiles
 
-# stow -v zsh      --target=$HOME
-stow -v zsh      --target=$HOME --dir=$DOTFILES/local
-
 # install Zim
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+# curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
